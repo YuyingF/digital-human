6983e0e848bf3af73d619cd4522ee8aea51c759d
yangzm <a2033242@126.com>
2023-07-18 15:31:38 +0800

    7.18日 前端+简易聊天窗口

--- src/main/java/com/example/digitalhuman/DigitalHumanApplication.java
+++ src/main/java/com/example/digitalhuman/DigitalHumanApplication.java
@@ -1,13 +1,21 @@
 package com.example.digitalhuman;
 
+import com.example.digitalhuman.Websocket.WebSocket;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DigitalHumanApplication {
 
+
     public static void main(String[] args) {
+
         SpringApplication.run(DigitalHumanApplication.class, args);
+
+
     }
+
+
 
 }
